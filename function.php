96fa05b9a262247bfbbcb20ba5ae16c4081d892c
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2021-10-07 14:25:04 +0300

    Update function.php

--- function.php
+++ function.php
@@ -51,7 +51,10 @@
     }
     file_put_contents($fileName,json_encode($categories));
   }
-  function getCategory(){
-     return json_decode(file_get_contents(__DIR__ . '/category.txt'),true);
+  function 
+     return json_decode(
+       file_get_contents(__DIR__ . '/categories.txt'),
+       true
+      );
   }
 ?>
