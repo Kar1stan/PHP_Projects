d65e91c98af426923cdedf2ed7198f5d81b53020
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2021-10-21 15:19:34 +0300

    Update category_form.php

--- category_form.php
+++ category_form.php
@@ -1,18 +1,18 @@
-<?php include_once __DIR__ . '/header.php'?>
+<?php include_once __DIR__ . '/header.php'<?php include_once __DIR__ . '/header.php'?>
 <div class='container'></div>
     <form action='category_create.php' method='POST'>
         <aside>
             <div class='mb-3'>
             <label class='form-label'>Name</label>
-            <input name='name' value='<?=old['name'] ?? null?>' type='text' class='form-control'>
+            <input name='name'  type='text' class='form-control'><?=old['name'] ?? null?></input>
             </div>
             <div class='mb-3'>
             <label class='form-label'>Id</label>
-            <input name='Id' value='<?=old['Parent_Id'] ?? null?>' type='text' class='form-control'>
+            <input name='Id'  type='text' class='form-control'><?=old['Id'] ?? null?></input>
             </div>
             <div class='mb-3'>
             <label class='form-label'>ParentId</label>
-            <input name='Parent_Id' type='text' class='form-control'><?=old['Id'] ?? null?></input>
+            <input name='Parent_Id' type='text' class='form-control'><?=old['Parent_Id'] ?? null?></input>
             </div>
             <button type='submit' class='btn btn-primary'>Submit</button>
         </aside>
